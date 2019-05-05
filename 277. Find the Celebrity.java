749e95c016123b0c0e500d35e327e9a4f4f79a76
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2019-05-04 17:33:45 -0700

    Create 277. Find the Celebrity.java

---
+++ 277. Find the Celebrity.java
@@ -0,0 +1,20 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        if (n < 2) return -1;
+        int possible = 0;
+        for (int i = 1; i < n; i++) {
+            if (knows(possible, i)) {
+                possible = i;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (possible != i && ((knows(possible, i) || !knows(i, possible)))) {
+                return -1;
+            }
+        }
+        return possible;
+    }
+}
