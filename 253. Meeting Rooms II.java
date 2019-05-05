18473b983813327200249df09cde9eaf327809df
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2019-05-05 01:02:16 -0700

    Create 253. Meeting Rooms II.java

---
+++ 253. Meeting Rooms II.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        if (intervals == null || intervals.length == 0)
+            return 0;
+        
+        int rooms = 0;
+        
+        Arrays.sort(intervals, (int[] a, int[] b) -> a[0] - b[0]);
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(intervals.length, (int[] a, int[] b) -> a[1] - b[1]);
+        
+        pq.offer(intervals[0]);
+        
+        for (int i = 1; i < intervals.length; i++)
+        {
+            int[] interval = pq.poll();
+            if (intervals[i][0] >= interval[1])
+                interval[1] = intervals[i][1];
+            else
+                pq.offer(intervals[i]);
+            pq.offer(interval);
+        }
+        
+        return pq.size();
+    }
+}
