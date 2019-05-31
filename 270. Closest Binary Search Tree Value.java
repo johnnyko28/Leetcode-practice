e5bda667d902eefdeb051d2d051c3680db3d0ea4
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2019-05-30 22:16:42 -0700

    Create 270. Closest Binary Search Tree Value.java

---
+++ 270. Closest Binary Search Tree Value.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int closestValue(TreeNode root, double target) {
+        int res = root.val;
+        while (root != null) {
+            if (Math.abs(target - root.val) < Math.abs(target - res)) {
+                res = root.val;
+            }
+            root = root.val > target ? root.left : root.right;
+        }
+        return res;
+    }
+}
