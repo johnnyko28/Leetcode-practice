2227441ffc78abebc90e128751060aefe7e72482
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2019-06-02 23:13:33 -0700

    Create 208. Implement Trie (Prefix Tree).java

---
+++ 208. Implement Trie (Prefix Tree).java
@@ -0,0 +1,64 @@
+class Trie {
+
+    /** Initialize your data structure here. */
+    class TrieNode {
+        TrieNode[] children;
+        boolean isWord;
+        String word;
+
+        public TrieNode() {
+            children = new TrieNode[26];
+            isWord = false;
+            word = "";
+        }
+    }
+    
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();        
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            int j = word.charAt(i) - 'a';
+            if (node.children[j] == null) {
+                node.children[j] = new TrieNode();
+            }
+            node = node.children[j];
+        }
+        node.isWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            int j = word.charAt(i) - 'a';
+            if (node.children[j] == null) return false;
+            node = node.children[j];
+        }
+        return node.isWord;        
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            int j = prefix.charAt(i) - 'a';
+            if (node.children[j] == null) return false;
+            node = node.children[j];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
