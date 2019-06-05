c753a40a612c22d2eabd89b891d7dc3de2f14e0a
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2019-06-04 23:02:59 -0700

    Update 277. Find the Celebrity.java

--- 277. Find the Celebrity.java
+++ 277. Find the Celebrity.java
@@ -4,17 +4,21 @@
 public class Solution extends Relation {
     public int findCelebrity(int n) {
         if (n < 2) return -1;
-        int candidate = 0;
+        int possible = 0;
         for (int i = 1; i < n; i++) {
-            if (knows(candidate, i)) {
-                candidate = i;
+            if (knows(possible, i)) {
+                possible = i;
             }
         }
         for (int i = 0; i < n; i++) {
-            if (candidate != i && ((knows(candidate, i) || !knows(i, candidate)))) {
+            if (possible != i && (knows(possible, i) || !knows(i, possible))) {
                 return -1;
             }
         }
-        return candidate;
+        return possible;
+    }
+    
+    public boolean kowns(int a, int b)  {
+        return true;
     }
 }
