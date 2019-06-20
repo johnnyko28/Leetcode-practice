6e6a8d8921c7af5a375f3db2abc6b48c91a4ff9f
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2019-06-20 15:17:24 -0700

    Create 94. Binary Tree Inorder Traversal.java

---
+++ 94. Binary Tree Inorder Traversal.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        helper(res, root);
+        return res;
+    }
+    public static void helper(List<Integer> res, TreeNode root) {
+        if (root == null) return;
+        helper(res, root.left);
+        res.add(root.val);
+        helper(res, root.right);
+    }
+}
