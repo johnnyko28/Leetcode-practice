cedebc6ddd2c21a2eea8227aa8b081aff71f14b7
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2020-01-09 21:35:05 -0800

    Create 134. Gas Station.java

---
+++ 134. Gas Station.java
@@ -0,0 +1,14 @@
+public int canCompleteCircuit(int[] gas, int[] cost) {
+        if (gas.length == 0 || cost.length == 0 || gas.length != cost.length) return -1;
+        int total = 0, sum = 0, start = 0;
+        for (int i = 0; i < gas.length; i++) {
+            total += (gas[i] - cost[i]);
+            if (sum < 0) {
+                sum = (gas[i] - cost[i]);
+                start = i;
+            } else {
+                sum += (gas[i] - cost[i]);
+            }
+        }
+        return total < 0 ? -1 : start;
+    }
