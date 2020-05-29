c0bd71c9d9c554034a83c1223ef16515a27eb7ef
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2020-05-28 17:14:39 -0700

    Create 20. Valid Parentheses.java

---
+++ 20. Valid Parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s == null || s.length() == 0) return true;
+        Stack<Character> stack = new Stack<>();
+        for (Character ch : s.toCharArray()) {
+            if (ch == '(') {
+                stack.push(')');
+            } else if (ch == '[') {
+                stack.push(']');
+            } else if (ch == '{') {
+                stack.push('}');
+            } else {
+                if (stack.isEmpty() || stack.pop() != ch) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
