981af79334b7786825d3c13272a3415109cf95b0
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2020-05-31 23:49:44 -0700

    Create 22. Generate Parentheses.java

---
+++ 22. Generate Parentheses.java
@@ -0,0 +1,21 @@
+public static List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        if (n == 0) return res;
+        helper(res, "", n, n);
+        return res;
+    }
+    public static void helper(List<String> res, String s, int left, int right) {
+        if (left > right) {
+            return;
+        }
+        if (left == 0 && right == 0) {
+            res.add(s);
+            return;
+        }
+        if (left > 0) {
+            helper(res, s + "(", left - 1, right);
+        }
+        if (right > 0) {
+            helper(res, s + ")", left, right - 1);
+        }
+    }
