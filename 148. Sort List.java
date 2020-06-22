515b60fc67c29bcab7c6aef8738f6d5257714c85
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2020-06-22 16:35:25 -0700

    Create 148. Sort List.java

---
+++ 148. Sort List.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode middle = getMiddle(head);
+        ListNode next = middle.next;
+        middle.next = null;
+        return merge(sortList(head), sortList(next));
+    }
+
+    private ListNode getMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    private ListNode merge(ListNode a, ListNode b) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (a != null && b != null) {
+            if (a.val <= b.val) {
+                cur.next = a;
+                a = a.next;
+            } else {
+                cur.next = b;
+                b = b.next;
+            }
+            cur = cur.next;
+        }
+        if (a == null) cur.next = b;
+        else cur.next = a;
+        return dummy.next;
+    }
+}
