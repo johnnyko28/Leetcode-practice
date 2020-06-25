eb27dc496e0d9c41af281f60b673948038d284d9
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2020-06-25 12:29:15 -0700

    Create 113. Path Sum II.java

---
+++ 113. Path Sum II.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        helper(res, new ArrayList<>(), root, sum);
+        return res;
+    }
+    public static void helper(List<List<Integer>> res, List<Integer> list, TreeNode root, int sum){
+        if (root == null) return;
+        list.add(root.val);
+        if (root.left == null && root.right == null){
+            if (sum == root.val) {
+                res.add(new ArrayList<>(list));
+            }
+        }
+        helper(res, list, root.left, sum - root.val);
+        helper(res, list, root.right, sum - root.val);
+        list.remove(list.size() - 1);
+    }
+}
