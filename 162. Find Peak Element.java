aaeadef36d8a307c139fd6ea15bff40aa56b2998
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2020-07-30 12:37:40 -0700

    Create 162. Find Peak Element.java

---
+++ 162. Find Peak Element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start + 1 < end){
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] > nums[mid + 1]){
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        if (nums[start] > nums[end]) return start;
+        return end;
+    }
+}
