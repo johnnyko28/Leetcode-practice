745f9627246628603112d06bb729a0e700879c2c
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2020-10-18 01:22:07 -0700

    Create 367. Valid Perfect Square.java

---
+++ 367. Valid Perfect Square.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if (num < 0) return false;
+       // if (num == 1) return true;
+        for (int i = 1; i <= num / i; i++){
+            if (i * i == num) return true;
+        }
+        return false;
+    }
+}
