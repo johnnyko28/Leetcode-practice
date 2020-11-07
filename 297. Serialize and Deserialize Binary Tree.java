559df672073df029225aaa0cc5c1b9b6ce2ae56c
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2020-11-06 21:36:34 -0800

    Create 297. Serialize and Deserialize Binary Tree.java

---
+++ 297. Serialize and Deserialize Binary Tree.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) return "";
+        StringBuilder res = new StringBuilder();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode cur = queue.poll();
+            if (cur == null){
+                res.append("null ");
+                continue;
+            }
+            res.append(cur.val + " ");
+            queue.offer(cur.left);
+            queue.offer(cur.right);
+        }
+        return res.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == "") return null;
+        String[] str = data.split(" ");
+        TreeNode root = new TreeNode(Integer.parseInt(str[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        for (int i = 1; i < str.length; i++) {
+            TreeNode cur = queue.poll();
+            if (!str[i].equals("null")) {
+                cur.left = new TreeNode(Integer.parseInt(str[i]));
+                queue.offer(cur.left);
+            }
+            if (!str[++i].equals("null")) {
+                cur.right = new TreeNode(Integer.parseInt(str[i]));
+                queue.offer(cur.right);
+            }
+        }
+        return root;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
