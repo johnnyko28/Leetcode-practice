eb61a3b5fcb4d677d13fab0794c11edb79e03178
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2020-11-07 15:02:18 -0800

    Create 207. Course Schedule.java

---
+++ 207. Course Schedule.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        int res = numCourses;
+        for (int[] pair : prerequisites) {
+            indegree[pair[0]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int pre = queue.poll();
+            res--;
+            for (int[] pair : prerequisites) {
+                if (pair[1] == pre) {
+                    indegree[pair[0]]--;
+                    if (indegree[pair[0]] == 0) {
+                        queue.offer(pair[0]);
+                    }
+                }
+            }
+        }
+        return res == 0;
+    }
+}
