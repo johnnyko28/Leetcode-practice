a831408a0a6ceef8ee6061a6815fef6af6a259d9
Chin-Chou Ko (Johnny) <johnnyko28@gmail.com>
2021-01-12 22:47:16 -0800

    Create 234. Palindrome Linked List.java

---
+++ 234. Palindrome Linked List.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) return true;
+        ListNode middle = findMiddle(head);
+        middle.next = reverse(middle.next);
+
+        ListNode p = head;
+        ListNode q = middle.next;
+        while (p != null && q != null) {
+            if (p.val != q.val) return false;
+            p = p.next;
+            q = q.next;
+        }
+        return true;
+    }
+    public ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
